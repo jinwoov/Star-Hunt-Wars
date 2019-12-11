a243efef5586ab6be861eb47ecfcb4c15d2ddec5
Jin Woo Kim <jinwoov@gmail.com>
2019-12-11 12:24:55 -0800

    added constructor on app.js

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 'use strict';
+/////////// Global variable///////////
+var darkAnswer = [];
+var lightAnswer = [];
+var questions = [];
+
+/////////////constructor/////////////
+
+function Strings (question, dark, light) {
+  this.question = question;
+  this.dark = dark;
+  this.light = light;
+
+  questions.push(this.question);
+  darkAnswer.push(this.dark);
+  lightAnswer.push(this.light);
+}
+
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+new Strings ('', '', '');
+
+
 
