8140a033a64f211ebd7806ab0de98928e5ee6ca2
enoch-strok <enochstrok@outlook.com>
2019-12-17 17:41:38 -0800

    First commit. Added initial global variables in js and text boxes in html.

--- js/indexapp.js
+++ js/indexapp.js
@@ -2,6 +2,11 @@
 var enterName = document.getElementById('name').value;
 var button = document.getElementById('button-container');
 
+  ////////////--Enoch--Adlib Input//////////
+var adlibQuestion1 = document.getElementById('adlibQuestionOne').value;
+var adlibQuestion2 = document.getElementById('adlibQuestionTwo').value;
+var adlibQuestion3 = document.getElementById('adlibQuestionThree').value;
+var adlibQuestion4 = document.getElementById('adlibQuestionFour').value;
 
 
 /////////////////////Save Name///////////////////////////////
