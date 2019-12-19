a34b2bc97ea5dd4f0db8e23387ff06c8af188a72
enoch-strok <enochstrok@outlook.com>
2019-12-18 20:54:14 -0800

    added second stage of stretch goal and it is rendering properly.

--- js/indexapp.js
+++ js/indexapp.js
@@ -1,26 +1,25 @@
 //////////////////////global/////////////////////////
-var enterName = document.getElementById('name').value;
+// var enterName = document.getElementById('name').value;
 var button = document.getElementById('button-container');
 
 ////////////--Enoch--Adlib Input//////////
-var adlibQuestion1 = document.getElementById('adlibQuestionOne').value;
-var adlibQuestion2 = document.getElementById('adlibQuestionTwo').value;
-var adlibQuestion3 = document.getElementById('adlibQuestionThree').value;
-var adlibQuestion4 = document.getElementById('adlibQuestionFour').value;
-
+// var adlibQuestion1 = document.getElementById('adlibQuestionOne').value;
+// var adlibQuestion2 = document.getElementById('adlibQuestionTwo').value;
+// var adlibQuestion3 = document.getElementById('adlibQuestionThree').value;
+// var adlibQuestion4 = document.getElementById('adlibQuestionFour').value;
 
 /////////////////////Save Name///////////////////////////////
 function saveData () {
-  enterName = document.getElementById('name').value;
+  var enterName = document.getElementById('name').value;
   localStorage.setItem('name', enterName);
 }
 
 ////////////--Enoch--Adlib Input//////////
 function saveAdlibData () {
-  adlibQuestion1 = document.getElementById('adlibQuestionOne').value;
-  adlibQuestion2 = document.getElementById('adlibQuestionTwo').value;
-  adlibQuestion3 = document.getElementById('adlibQuestionThree').value;
-  adlibQuestion4 = document.getElementById('adlibQuestionFour').value;
+  var adlibQuestion1 = document.getElementById('adlibQuestionOne').value;
+  var adlibQuestion2 = document.getElementById('adlibQuestionTwo').value;
+  var adlibQuestion3 = document.getElementById('adlibQuestionThree').value;
+  var adlibQuestion4 = document.getElementById('adlibQuestionFour').value;
   localStorage.setItem('Question1', adlibQuestion1);
   localStorage.setItem('Question2', adlibQuestion2);
   localStorage.setItem('Question3', adlibQuestion3);
